cd13df7da1a3c9e534b4655c659f0ce56f0065b7
seonghwi lee <seonghwi.lee@dream-academy.io>
2023-03-19 23:27:04 +0900

    [UPDATE] pass testAddLiquidity3,4,5

--- src/Dex.sol
+++ src/Dex.sol
@@ -1,13 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {CustomERC20} from "../test/Dex.t.sol";
+
+import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract Dex {
     mapping(address => uint256[2]) liquidityPool;
+    IERC20 private tokenX;
+    IERC20 private tokenY;
+
+    address private owner;
     uint256[2] pool;
     uint256 k;
 
-    constructor(address _token1, address _token2) {
-        k = _token1.balance * _token2.balance;
+    constructor(address _tokenX, address _tokenY) {
+        k = _tokenX.balance * _tokenY.balance;
+        owner = msg.sender;
+        tokenX = IERC20(_tokenX);
+        tokenY = IERC20(_tokenY);
     }
 
     function swap(
@@ -22,6 +33,23 @@ contract Dex {
         uint256 minimumLPTokenAmount
     ) external returns (uint256 LPTokenAmount) {
         require(tokenXAmount == tokenYAmount);
+        unchecked {
+            require(
+                tokenX.allowance(msg.sender, address(this)) >= tokenXAmount &&
+                    tokenY.allowance(msg.sender, address(this)) >= tokenYAmount,
+                "ERC20: insufficient allowance"
+            );
+        }
+        require(
+            tokenX.balanceOf(msg.sender) >= tokenXAmount &&
+                tokenY.balanceOf(msg.sender) >= tokenYAmount,
+            "ERC20: transfer amount exceeds balance"
+        );
+
+        if (minimumLPTokenAmount > tokenXAmount) revert();
+
+        tokenX.transferFrom(msg.sender, address(this), tokenXAmount);
+        tokenY.transferFrom(msg.sender, address(this), tokenYAmount);
 
         return tokenXAmount;
     }
