37dacc9861c810ee2d5cf07c731286cccaf8f5a8
seonghwi lee <seonghwi.lee@dream-academy.io>
2023-03-19 22:09:23 +0900

    [UPDATE] skeleton DreamAcademyLending.sol

--- src/DreamAcademyLending.sol
+++ src/DreamAcademyLending.sol
@@ -3,4 +3,28 @@ pragma solidity ^0.8.13;
 
 interface IPriceOracle {}
 
-contract DreamAcademyLending {}
+contract DreamAcademyLending {
+    constructor(IPriceOracle dreamOracle, address tokenAddress) {}
+
+    function initializeLendingProtocol(address tokenAddress) external payable {}
+
+    function getAccruedSupplyAmount(
+        address tokenAddress
+    ) external returns (uint256 amount) {}
+
+    function deposit(address tokenAddress, uint256 amount) external payable {}
+
+    function borrow(address tokenAddress, uint256 amount) external {}
+
+    function repay(address tokenAddress, uint256 amount) external {}
+
+    function liquidate(
+        address user,
+        address tokenAddress,
+        uint256 amount
+    ) external {}
+
+    function withdraw(address tokenAddress, uint256 amount) external {}
+
+    receive() external payable {}
+}
