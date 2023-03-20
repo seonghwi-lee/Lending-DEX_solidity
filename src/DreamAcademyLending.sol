18ea414823970e1f9971630dd1060435cf9e64c2
seonghwi lee <seonghwi.lee@dream-academy.io>
2023-03-20 15:57:48 +0900

    [UPDATE] pass testDepoist

--- src/DreamAcademyLending.sol
+++ src/DreamAcademyLending.sol
@@ -1,18 +1,54 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-interface IPriceOracle {}
+import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "forge-std/console.sol";
+
+interface IPriceOracle {
+    function getPrice(address token) external view returns (uint256);
+
+    function setPrice(address token, uint256 price) external;
+}
 
 contract DreamAcademyLending {
-    constructor(IPriceOracle dreamOracle, address tokenAddress) {}
+    IERC20 usdc;
+    IPriceOracle dreamOracle;
+    mapping(address => uint256) _balance;
 
-    function initializeLendingProtocol(address tokenAddress) external payable {}
+    constructor(IPriceOracle _dreamOracle, address _tokenAddress) {
+        dreamOracle = _dreamOracle;
+        usdc = IERC20(_tokenAddress);
+    }
+
+    function initializeLendingProtocol(address tokenAddress) external payable {
+        require(tokenAddress != address(0));
+        require(msg.value > 0);
+
+        _balance[msg.sender] += msg.value;
+    }
+
+    function balance() public view returns (uint256 amount) {
+        return _balance[msg.sender];
+    }
 
     function getAccruedSupplyAmount(
         address tokenAddress
     ) external returns (uint256 amount) {}
 
-    function deposit(address tokenAddress, uint256 amount) external payable {}
+    function deposit(address tokenAddress, uint256 amount) external payable {
+        if (tokenAddress == address(0)) {
+            require(msg.value > 0);
+            require(msg.value >= amount);
+            _balance[msg.sender] += amount;
+        } else {
+            require(amount <= usdc.allowance(msg.sender, address(this)));
+            usdc.transferFrom(
+                msg.sender,
+                address(this),
+                amount + _balance[msg.sender]
+            );
+        }
+    }
 
     function borrow(address tokenAddress, uint256 amount) external {}
 
